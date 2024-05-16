b5b1828f7255c28f22b96f3e4a3d3a93253a7eed
Rory O'Riordan <47453846+roryor11@users.noreply.github.com>
2024-05-16 14:17:46 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 # adding in the fruit options from the table created in the DABW worksheet
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
