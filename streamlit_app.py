decec2cf108b303614f49acb537c8fd96ffcbcab
Rory O'Riordan <47453846+roryor11@users.noreply.github.com>
2024-05-16 12:06:03 +1000

    Update streamlit_app.py
    connected changes needed because of move from sis to snis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -19,7 +18,8 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 
 
 # adding in the fruit options from the table created in the DABW worksheet
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = snx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
